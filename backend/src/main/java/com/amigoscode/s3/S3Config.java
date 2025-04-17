e4ac932e92256bdf53b5e4c7be63052a32b56859
tannercantlay <tannercantlay@gmail.com>
2025-04-16 22:59:53 -0400

    configured s3 credentials and connection

---
+++ backend/src/main/java/com/amigoscode/s3/S3Config.java
@@ -0,0 +1,22 @@
+package com.amigoscode.s3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.regions.Region;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+    // Configure the S3 client with the desired region and endpoint
+    @Bean
+    public S3Client s3Client() {   
+        S3Client client = S3Client.builder()
+                .region(Region.of("awsRegion")) //configure the region from resources/application.yml
+                .build();
+        return client;
+    }                    
+}
